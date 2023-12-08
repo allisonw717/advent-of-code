d86e79c64d98e2eebb835eea909920fe4fbee1ec
Allison Wei <weiallison1999@gmail.com>
2023-12-07 20:08:24 -0500

    cursed day 5 part 2

--- src/day5.py
+++ src/day5.py
@@ -48,15 +48,48 @@ def part_one():
     print(min(locations))
 
 
-def get_next_range(ranges, map):
-    dests = [] 
-    for my_range in ranges:
-        for source in map.keys():
-            if my_range[0] >= source[0] and my_range[1] <= source[1]:
-                dests.append((my_range[0] + map[source], my_range[1] + map[source]))
-    return dests
+def get_overlap(x, y):
+    left = max(x[0], y[0])
+    right = min(x[1], y[1])
+    if left <= right:
+        return (left,right)
+    return None
 
+def test_get_overlap():
+    print(get_overlap((0,10), (0,0)))
+    print(get_overlap((0,10), (5,15)))
+    print(get_overlap((5,15), (0,10)))
+    print(get_overlap((1,5), (2,3)))
+    print(get_overlap((2,3), (1,5)))
 
+def get_next_range(range, map):
+    '''
+    range = original range
+    map = k: ranges that change, v: how much it changes
+    returns list of new range applying changes where needed
+    '''
+    result = [] 
+    overlaps = [] 
+    for source in map.keys():
+        overlap = get_overlap(range, source)
+        if overlap:
+            overlaps.append(overlap)
+            result.append((overlap[0] + map[source], overlap[1] + map[source]))
+    if not overlaps:
+        return [range] 
+    sorted_overlaps = sorted(overlaps)
+    for i, overlap in enumerate(sorted_overlaps[:-1]):
+        after_end = overlap[1] + 1
+        before_next_start = sorted_overlaps[i+1][0] - 1
+        if after_end <= before_next_start:
+            result.append((after_end, before_next_start))
+    if range[0] < sorted_overlaps[0][0]:
+        result.append((range[0],sorted_overlaps[0][0] - 1))
+    if range[1] > sorted_overlaps[-1][1]:
+        result.append((sorted_overlaps[-1][1] + 1, range[1]))
+    
+    return result 
+        
 def part_two():
     ranges = []
     seeds = lines[0].split(":")[1].split()
@@ -64,24 +97,32 @@ def part_two():
     while i < len(seeds) - 1:
         ranges.append((int(seeds[i]), int(seeds[i]) + int(seeds[i+1]) - 1))
         i += 2
-    print(ranges)
-    my_map = {} 
     for line in lines[2:]:
         if line in ['\n', '\r\n']:
-            continue
+            new_ranges = []
+            for range in ranges:
+                new_ranges.extend(get_next_range(range, my_map))
+            ranges = new_ranges
         elif "map" in line:
-            print("map yeah")
+            my_map = {} 
         else: 
             range_info = [int(x) for x in line.split()]
             my_map[(range_info[1], range_info[1] + range_info[2] - 1)] = range_info[0] - range_info[1]
-            
-    print(my_map)
-    print("sort" + str(sorted(my_map.keys())))
-    print(get_next_range(ranges, my_map))
 
-            
+    #I don't know why it wasn't processing the last one lol 
+    new_ranges = []
+    for range in ranges:
+        new_ranges.extend(get_next_range(range, my_map))
+    ranges = new_ranges
 
+    minimum = min(ranges)
+    print("PART TWO ANSWER: " + str(minimum[0]))
+            
+#test_get_overlap()
+part_one()
 part_two()
+#391544129 is too high 
+#13606986 is too high 
 
         
 
