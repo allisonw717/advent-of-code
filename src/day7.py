c7448a188d1a94715e654d8f5c7dd9a1ab7c3962
Allison Wei <weiallison1999@gmail.com>
2023-12-09 23:08:13 -0500

    broken 7 pt 2

--- src/day7.py
+++ src/day7.py
@@ -20,18 +20,17 @@ class hand:
                     self.cards.append(self.letters[card])
             else:
                 self.cards.append(int(card))
+        self.original_cards = [x for x in self.cards]
         self.bid = int(bid)
-        self.strength = self.get_strengh()
+        self.strength = self.get_strength()
     
     def __str__(self):
-        string = str(self.cards) + ": "+ str(self.bid)
+        string = str(self.original_cards) + ": "+ str(self.bid)
         return string
     
     #five: 7, four:6, full:5, three:4, two:3, one:2, high:1
-    def get_strengh(self):
+    def get_strength_no_joker(self):
         counts = sorted(Counter(self.cards).values(), reverse=True)
-        if self.joker and 1 in self.cards:
-             counts[0] += self.cards.count(1)
         if counts[0] == 5:
             return 7
         if counts[0] == 4:
@@ -44,13 +43,30 @@ class hand:
             if counts[1] == 2:
                 return 3
             return 2
-        return 1   
+        return 1
     
+    def get_strength(self):
+        if self.joker and 1 in self.cards:
+            #card with most occurences first, then on 
+            cards_by_freq = sorted(Counter(self.cards), reverse=True)
+            if cards_by_freq[0] != 1:
+                assert Counter(self.cards).most_common != 1
+                self.cards = [cards_by_freq[0] if item == 1 else item for item in self.cards]
+            elif len(cards_by_freq) != 1:
+                assert Counter(self.cards).most_common == 1
+                self.cards = [cards_by_freq[1] if item == 1 else item for item in self.cards]
+            else:
+                assert self.cards == [1] * 5
+        return self.get_strength_no_joker()
+
     def __gt__(self, other):
         if self.strength == other.strength:
             for i in range(0,5):
-                if self.cards[i] != other.cards[i]:
-                    return self.cards[i] > other.cards[i]
+                assert self.original_cards[i] != 11, f"self.original_cards had 11 - {self.original_cards}"
+                assert other.original_cards[i] != 11, f"other.original_cards had 11 - {other.original_cards}"
+                if self.original_cards[i] != other.original_cards[i]:
+                    return self.original_cards[i] > other.original_cards[i]
+            assert False
             return False
         return self.strength > other.strength
 
@@ -62,9 +78,13 @@ for line in lines:
     joker_hands.append(hand(cards,bid,True))
 
 #PART 1
-winnings = 0 
-for i, hand in enumerate(sorted(hands)):
-    winnings += hand.bid * (i + 1)
+# winnings = 0 
+# for i, hand in enumerate(sorted(hands)):
+#     winnings += hand.bid * (i + 1)
+# if winnings == 250898830:
+#     print("congrats didn't break part one")
+# else:
+#     print("broke part 1 smh")
 
 #PART 2
 winnings_with_joker = 0 
@@ -72,5 +92,7 @@ for i, hand in enumerate(sorted(joker_hands)):
     print(hand)
     winnings_with_joker += hand.bid * (i + 1)
 
-print(winnings)
 print(winnings_with_joker)
+
+#251976591 too low 
+#251828497 ... got lower ??? 
