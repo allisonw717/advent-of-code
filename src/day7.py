494bf2b01eff8c3172f7bf31e29ed966d7419fdb
Allison Wei <weiallison1999@gmail.com>
2023-12-09 23:18:57 -0500

    fixed day 7

--- src/day7.py
+++ src/day7.py
@@ -48,25 +48,20 @@ class hand:
     def get_strength(self):
         if self.joker and 1 in self.cards:
             #card with most occurences first, then on 
-            cards_by_freq = sorted(Counter(self.cards), reverse=True)
+            #cards_by_freq = sorted(Counter(self.cards), reverse=True)
+            counts = Counter(self.cards)
+            cards_by_freq = sorted(counts, key=counts.get, reverse=True)
             if cards_by_freq[0] != 1:
-                assert Counter(self.cards).most_common != 1
                 self.cards = [cards_by_freq[0] if item == 1 else item for item in self.cards]
             elif len(cards_by_freq) != 1:
-                assert Counter(self.cards).most_common == 1
                 self.cards = [cards_by_freq[1] if item == 1 else item for item in self.cards]
-            else:
-                assert self.cards == [1] * 5
         return self.get_strength_no_joker()
 
     def __gt__(self, other):
         if self.strength == other.strength:
             for i in range(0,5):
-                assert self.original_cards[i] != 11, f"self.original_cards had 11 - {self.original_cards}"
-                assert other.original_cards[i] != 11, f"other.original_cards had 11 - {other.original_cards}"
                 if self.original_cards[i] != other.original_cards[i]:
                     return self.original_cards[i] > other.original_cards[i]
-            assert False
             return False
         return self.strength > other.strength
 
@@ -78,18 +73,17 @@ for line in lines:
     joker_hands.append(hand(cards,bid,True))
 
 #PART 1
-# winnings = 0 
-# for i, hand in enumerate(sorted(hands)):
-#     winnings += hand.bid * (i + 1)
-# if winnings == 250898830:
-#     print("congrats didn't break part one")
-# else:
-#     print("broke part 1 smh")
+winnings = 0 
+for i, hand in enumerate(sorted(hands)):
+    winnings += hand.bid * (i + 1)
+if winnings == 250898830:
+    print("congrats didn't break part one")
+else:
+    print("broke part 1 smh")
 
 #PART 2
 winnings_with_joker = 0 
 for i, hand in enumerate(sorted(joker_hands)):
-    print(hand)
     winnings_with_joker += hand.bid * (i + 1)
 
 print(winnings_with_joker)
